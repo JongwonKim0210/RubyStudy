2335e70369792c336ff8b5d66721f4c5c4cf5185
JongwonKim0210 <71743704+JongwonKim0210@users.noreply.github.com>
2020-10-20 18:01:34 +0900

    Update routes.rb

--- routes.rb
+++ routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 	
-	get '/' => 'home#index'
+  get '/' => 'home#index'
 	
-  	get 'home/index'
-  	post 'home/create'
-  	post 'home/update/:id' => 'home#update'
-  	get 'home/delete/:id' => 'home#delete'
-  	get 'home/create_form' => 'home#create_form'
-  	get 'home/update_form/:id' => 'home#update_form'
+  get 'home/index'
+  post 'home/create'
+  post 'home/update/:id'		=> 'home#update'
+  get 'home/delete/:id'			=> 'home#delete'
+  get 'home/create_form'
+  get 'home/update_form/:id'		=> 'home#update_form'
 	
-	post 'reply/create/:article_id' => 'home#reply_create'
+  post 'reply/create/:article_id'	=> 'home#reply_create'
+  get 'reply/delete/:reply_id'		=> 'home#reply_delete'
+  get 'reply/update_form/:reply_id'	=> 'home#reply_update_form'
+  post 'reply/update/:reply_id'		=> 'home#reply_update'
 	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	
